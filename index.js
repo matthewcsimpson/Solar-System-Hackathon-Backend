769647da42ed5f63db1063e38a4a46db69e1175b
Matthew Simpson <mcsimpson+gh@gmail.com>
2023-03-27 13:16:12 -0700

    Initial Setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 5555;
+
+// console log all requests
+app.use((req, _res, next) => {
+  console.info(`Incoming request from ${req.originalUrl}`);
+  next();
+});
+
+// GET the main index.
+app.get("/", (_req, res) => {
+  res.json({ status: "running" });
+});
+
+// Listening
+app.listen(PORT, () => {
+  console.info(`Server is running on port ${PORT}`);
+});
