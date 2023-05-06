94aa49ca0b0211dd11b55c6c0744cc565f4b57c1
Matthew Simpson <mcsimpson+gh@gmail.com>
2023-05-06 14:50:57 -0700

    more detailed all bodies endpoint

--- controllers/getAllBodiesController.js
+++ controllers/getAllBodiesController.js
@@ -75,4 +75,85 @@ const getAllBodies = async (req, res) => {
   res.status(200).json(returnData);
 };
 
-module.exports = { getAllBodies };
+
+const getAllBodiesDetailed = async (req, res) => {
+  let starData = await knex("star")
+    .select("star_id", "englishName", "meanRadius")
+    .then((starData) => {
+      return starData;
+    })
+    .catch((err) => {
+      console.error("getAllBodies", err);
+      res.json({
+        message: "Something went wrong getting STAR data",
+        error: err,
+      });
+    });
+
+  let planetsData = await knex("planets")
+    .select(
+      "planet_id",
+      "englishName",
+      "meanRadius",
+      "perihelion",
+      "aphelion",
+      "sideralOrbit",
+      "sideralRotation",
+      "inclination",
+      "bodyType",
+      "planetType",
+      "star_id"
+    )
+    .orderBy(req.query.sortColumn || "perihelion")
+    .then((planetsData) => {
+      return planetsData;
+    })
+    .catch((err) => {
+      console.error("getAllBodies", err);
+      res.json({
+        message: "Something went wrong getting PLANET data",
+        error: err,
+      });
+    });
+
+  let moonData = await knex("moons")
+    .select({
+      moon_id: "moons.moon_id",
+      englishName: "moons.englishName",
+      meanRadius: "moons.meanRadius",
+      perihelion: "moons.perihelion",
+      aphelion: "moons.aphelion",
+      sideralOrbit: "moons.sideralOrbit",
+      sideralRotation: "moons.sideralRotation",
+      inclination: "moons.inclination",
+      planet_id: "moons.planet_id",
+      planetEnglishName: "planets.englishName",
+    })
+    .join("planets", "moons.planet_id", "planets.planet_id")
+    .orderBy("moons.planet_id")
+    .orderBy(
+      req.query.sortColumn
+        ? `moons.${req.query.sortColumn}`
+        : "moons.perihelion"
+    )
+    .then((moonData) => {
+      return moonData;
+    })
+    .catch((err) => {
+      console.error("getAllBodies", err);
+      res.json({
+        message: "Something went wrong getting MOON data",
+        error: err,
+      });
+    });
+
+  let returnData = {
+    star: starData,
+    planets: planetsData,
+    moons: moonData,
+  };
+
+  res.status(200).json(returnData);
+};
+
+module.exports = { getAllBodies, getAllBodiesDetailed };
--- routes/stellarRouter.js
+++ routes/stellarRouter.js
@@ -2,20 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const { getAllBodies } = require("../controllers/getAllBodiesController");
+const {
+  getAllBodies,
+  getAllBodiesDetailed,
+} = require("../controllers/getAllBodiesController");
 const {
   getAllPlanets,
   getAllDwarfPlanets,
   getAllLargePlanets,
-  getAllPlanetsDetail
+  getAllPlanetsDetail,
 } = require("../controllers/getAllPlanetsController");
 const { getPlanetById } = require("../controllers/getPlanetByIdController");
 const { getStar } = require("../controllers/getStarController");
-const { getAllMoons, getAllMoonsByPlanet } = require("../controllers/getAllMoonsController");
+const {
+  getAllMoons,
+  getAllMoonsByPlanet,
+} = require("../controllers/getAllMoonsController");
 const { getMoonById } = require("../controllers/getMoonByIdController");
 
 // GET all bodies in the stellar system.
 router.get("/", getAllBodies);
+router.get("/detailed", getAllBodiesDetailed);
 
 // GET the details for the star
 router.get("/star", getStar);
@@ -24,7 +31,7 @@ router.get("/star", getStar);
 router.get("/planets", getAllPlanets);
 router.get("/planets/dwarf", getAllDwarfPlanets);
 router.get("/planets/planet", getAllLargePlanets);
-router.get('/planetsDetail', getAllPlanetsDetail);
+router.get("/planets/detail", getAllPlanetsDetail)
 
 // GET a planet by its planet_id
 router.get("/planet/:planet_id", getPlanetById);
@@ -32,10 +39,10 @@ router.get("/planet/:planet_id", getPlanetById);
 // GET all moons
 router.get("/moons", getAllMoons);
 
-// GET all moons by the planet they orbit. 
-router.get('/moons/:planet_id', getAllMoonsByPlanet)
+// GET all moons by the planet they orbit.
+router.get("/moons/:planet_id", getAllMoonsByPlanet);
 
-// GET a single moon by its ID. 
-router.get('/moon/:moon_id', getMoonById)
+// GET a single moon by its ID.
+router.get("/moon/:moon_id", getMoonById);
 
 module.exports = router;
